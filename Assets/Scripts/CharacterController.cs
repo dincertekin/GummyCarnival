c5fce60779bcc9bc2cc127c2a4eebcda8bbacbc8
Lawnless <69431018+Lawnless@users.noreply.github.com>
2023-05-19 22:06:15 +0300

    first push

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+	public float movementSpeed = 3f;
+	private Rigidbody rb;
+	private Vector3 movement;
+    public float interactionDistance = 3f;
+
+	public Vector3[] boothCoords = new Vector3[] {
+		new Vector3(5.392f, 0.313f, -10.281f),
+		new Vector3(15.01f, 0.325f, -10.29f),
+		new Vector3(23.93f, 0.325f, -10.29f)
+	};
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.E))
+		{
+			if (Vector3.Distance(transform.position, boothCoords[0]) <= interactionDistance)
+			{
+				Debug.Log("Stand 1 yakınında E basıldı.");
+			}
+			else if (Vector3.Distance(transform.position, boothCoords[1]) <= interactionDistance)
+			{
+				Debug.Log("Stand 2 yakınında E basıldı.");
+			}
+			else if (Vector3.Distance(transform.position, boothCoords[2]) <= interactionDistance)
+			{
+				Debug.Log("Stand 3 yakınında E basıldı.");
+			}
+		} else {
+			float horizontalInput = Input.GetAxis("Horizontal");
+			float verticalInput = Input.GetAxis("Vertical");
+
+			movement = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Scripts/IntroTextFade.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroTextFadeOut : MonoBehaviour
+{
+    public Text introText;
+    public float fadeDuration = 2f;
+
+    private void Start()
+    {
+        introText.color = Color.clear;
+        StartCoroutine(FadeInText());
+    }
+
+    private System.Collections.IEnumerator FadeInText()
+    {
+        float elapsedTime = 0f;
+        Color initialColor = Color.clear;
+        Color targetColor = Color.white;
+
+        while (elapsedTime < 1f)
+        {
+            elapsedTime += Time.deltaTime / fadeDuration;
+            introText.color = Color.Lerp(initialColor, targetColor, elapsedTime);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(2f);
+
+        StartCoroutine(FadeOutText());
+    }
+
+    private System.Collections.IEnumerator FadeOutText()
+    {
+        float elapsedTime = 0f;
+        Color initialColor = Color.white;
+        Color targetColor = Color.clear;
+
+        while (elapsedTime < 1f)
+        {
+            elapsedTime += Time.deltaTime / fadeDuration;
+            introText.color = Color.Lerp(initialColor, targetColor, elapsedTime);
+            yield return null;
+        }
+
+        SceneManager.LoadScene("MainGameScene");
+    }
+}
