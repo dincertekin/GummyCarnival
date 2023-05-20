a26969b7d714f8e0875e4df744e97ae6ddb0a1ac
Lawnless <69431018+Lawnless@users.noreply.github.com>
2023-05-20 18:36:11 +0300

    .

---
+++ Assets/Scripts/MenuItem.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuItem : MonoBehaviour
+{
+    public Sprite deetaSakizKutuKapali;
+	public Sprite deetaSakizKutuAcik;
+	private Image image;
+
+    void Start()
+	{
+		image = GetComponent<Image>();
+	}
+
+    public void openThePandorasBox() {
+        image.sprite = deetaSakizKutuAcik;
+    }
+
+    public void closeThePandorasBox() {
+        image.sprite = deetaSakizKutuKapali;
+    }
+}
